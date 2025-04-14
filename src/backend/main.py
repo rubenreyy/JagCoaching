a58affbd76c23e3b16be73de4a4a4c55f4db0191
rubenreyy <ruben8reyed@gmail.com>
2025-04-14 03:01:47 -0500

    adjusted logging

--- src/backend/main.py
+++ src/backend/main.py
@@ -25,7 +25,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler('app.log')
+        logging.FileHandler('/tmp/app.log')
     ]
 )
 
