9c07d118c4e9b71932346d8a9a0711b488d69429
rubenreyy <ruben8reyed@gmail.com>
2025-04-29 23:40:11 -0500

    ???

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import dotenv from 'dotenv';
-dotenv.config()
+
 
 
 // [ ] : add proxy for other routes if necessary
