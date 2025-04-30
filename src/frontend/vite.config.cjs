e27ddec9ab888aa6ca1f64c72e0a18b4a2bc7102
rubenreyy <ruben8reyed@gmail.com>
2025-04-29 23:34:34 -0500

    adjust config

--- src/frontend/vite.config.cjs
+++ src/frontend/vite.config.cjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import dotenv from 'dotenv'
+const dotenv = require('dotenv');
 dotenv.config()
 
 
